3aa313294c479e65acfea58235bba41d87426c62
Gabriel Frasson <bielfc27@gmail.com>
2022-10-19 15:37:52 -0300

    Server initial config

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,18 @@
+import cors from "cors";
 import express from "express";
+import "express-async-errors";
+
+import "@shared/container";
+
+import { errorHandler } from "@shared/errors/errorHandler";
+
+import { router } from "./routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+app.use(errorHandler);
 
 export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,7 @@
+import { createConnection } from "../typeorm";
 import { app } from "./app";
+
+createConnection();
 
 app.listen(3333, () => {
     console.log("Server is running!");
