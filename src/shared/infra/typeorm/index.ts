cd84f956b44f33f040b50a7c3829f7e7240ae665
Gabriel Frasson <bielfc27@gmail.com>
2022-10-19 19:41:26 -0300

    Register new Entities and migrations

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,15 @@
+import "dotenv/config";
 import { DataSource, DatabaseType } from "typeorm";
 
 import { Bull } from "@modules/bulls/infra/typeorm/entities/Bull";
-
-import "dotenv/config";
-
-import { FormItem } from "@modules/forms/infra/typeorm/entities/FormItem";
+import { Form } from "@modules/selectionProfiles/infra/typeorm/entities/Form";
+import { ProfileItem } from "@modules/selectionProfiles/infra/typeorm/entities/ProfileItem";
+import { SelectionProfile } from "@modules/selectionProfiles/infra/typeorm/entities/SelectionProfile";
 
 import { CreateBulls1666212336611 } from "./migrations/1666212336611-CreateBulls";
-import { CreateFormItems1666213532115 } from "./migrations/1666213532115-CreateFormItems";
+import { CreateProfileItems1666213532115 } from "./migrations/1666213532115-CreateProfileItems";
+import { CreateSelectionProfiles1666213532125 } from "./migrations/1666213532125-CreateSelectionProfiles";
+import { CreateForms1666216312481 } from "./migrations/1666216312481-CreateForms";
 
 const dbType: DatabaseType = "mysql";
 
@@ -20,8 +22,13 @@ const AppDataSource = new DataSource({
     port: Number(process.env.TYPEORM_PORT),
     synchronize: false,
     logging: false,
-    entities: [Bull, FormItem],
-    migrations: [CreateBulls1666212336611, CreateFormItems1666213532115],
+    entities: [Bull, ProfileItem, SelectionProfile, Form],
+    migrations: [
+        CreateBulls1666212336611,
+        CreateProfileItems1666213532115,
+        CreateSelectionProfiles1666213532125,
+        CreateForms1666216312481,
+    ],
 });
 
 export function createConnection(): Promise<DataSource> {
