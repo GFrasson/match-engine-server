73cd02be49d4e71311ee0e731b6073eabbcd8806
Gabriel Frasson <bielfc27@gmail.com>
2022-10-19 19:40:51 -0300

    CreateForms migration

---
+++ src/shared/infra/typeorm/migrations/1666216312481-CreateForms.ts
@@ -0,0 +1,59 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+import { Operator } from "@modules/selectionProfiles/infra/typeorm/enums/Operator";
+
+export class CreateForms1666216312481 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "forms",
+                columns: [
+                    {
+                        type: "uuid",
+                        name: "id",
+                        isPrimary: true,
+                    },
+                    {
+                        type: "uuid",
+                        name: "selection_profile_id",
+                    },
+                    {
+                        type: "uuid",
+                        name: "profile_item_id",
+                    },
+                    {
+                        type: "numeric",
+                        name: "value",
+                    },
+                    {
+                        type: "enum",
+                        name: "operator",
+                        enum: Object.values(Operator),
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKSelectionProfile",
+                        referencedTableName: "selection_profiles",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["selection_profile_id"],
+                        onDelete: "RESTRICT",
+                        onUpdate: "RESTRICT",
+                    },
+                    {
+                        name: "FKProfileItem",
+                        referencedTableName: "profile_items",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["profile_item_id"],
+                        onDelete: "RESTRICT",
+                        onUpdate: "RESTRICT",
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("forms");
+    }
+}
