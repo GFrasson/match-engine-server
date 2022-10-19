3b4ae9324df377e71847a3707ab2c8eedfcef6a9
Gabriel Frasson <bielfc27@gmail.com>
2022-10-19 19:39:56 -0300

    Add columns related to genealogy to Bulls migration

--- src/shared/infra/typeorm/migrations/1666212336611-CreateBulls.ts
+++ src/shared/infra/typeorm/migrations/1666212336611-CreateBulls.ts
@@ -64,10 +64,53 @@ export class CreateBulls1666212336611 implements MigrationInterface {
                         name: "beta_casein",
                         isNullable: true,
                     },
+                    {
+                        type: "uuid",
+                        name: "first_level_parent",
+                        isNullable: true,
+                    },
+                    {
+                        type: "uuid",
+                        name: "second_level_parent",
+                        isNullable: true,
+                    },
+                    {
+                        type: "uuid",
+                        name: "third_level_parent",
+                        isNullable: true,
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKFirstLevelParent",
+                        referencedTableName: "bulls",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["first_level_parent"],
+                        onDelete: "RESTRICT",
+                        onUpdate: "RESTRICT",
+                    },
+                    {
+                        name: "FKSecondLevelParent",
+                        referencedTableName: "bulls",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["second_level_parent"],
+                        onDelete: "RESTRICT",
+                        onUpdate: "RESTRICT",
+                    },
+                    {
+                        name: "FKThirdLevelParent",
+                        referencedTableName: "bulls",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["third_level_parent"],
+                        onDelete: "RESTRICT",
+                        onUpdate: "RESTRICT",
+                    },
                 ],
             })
         );
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {}
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("bulls");
+    }
 }
